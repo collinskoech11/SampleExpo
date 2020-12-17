d2c2c0dca39245699b2fc1c0866df9df2f52338e
Collins Koech <collinskoechck34@gmail.com>
2020-12-18 00:13:14 +0300

    header update

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Header } from 'react-native/Libraries/NewAppScreen';
-import Header from './src/components/Header';
 
 export default function App() {
   return (
